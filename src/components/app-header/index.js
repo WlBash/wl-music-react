f7d6f69a95ad3965f44e1adad4d4261f41a52d53
Ramenbear <yx19990102@163.com>
2020-12-07 20:26:05 +0800

    导航栏完成

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,21 +1,56 @@
 import React, { memo } from 'react'
 
+import { NavLink } from "react-router-dom";
+
+import { headerLinks } from "@/services/local-data";
+import { Input } from "antd";
+import { SearchOutlined } from '@ant-design/icons';
 import{
     AppHeaderWrapper,
-    headerLeft,
-    headerRight
+    HeaderLeft,
+    HeaderRight
 } from './style'
 
 
 export default memo(function WlAppHeader() {
+
+    const showItem = (item, index) => {
+        if (index < 3) {
+            return (
+              <NavLink to={item.link}>
+                {item.title}
+                <i className="sprite_01 icon"></i>
+              </NavLink>
+            )
+          } else {
+            return <a href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
+          }
+    }
+
     return (
         <AppHeaderWrapper>
             <div className="content wrap-v1">
-                <headerLeft>
+                <HeaderLeft>
                     <a className="logo sprite_01" href="#/">网易云音乐</a>
-                </headerLeft>
-                <headerRight></headerRight>
+                    <div className="select-list">
+                    {
+                        headerLinks.map((item, index) => {
+                            return(
+                                <div className="select-item" key={item.title}>
+                                    {showItem(item, index)}
+                                </div>                        
+                            )
+                        })
+                    }
+                    </div>
+                </HeaderLeft>
+                <HeaderRight>
+                <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />} />
+          <div className="center">创作者中心</div>
+          <div className="">登录</div>
+                </HeaderRight>             
             </div>
+            <div className="divider"></div>
         </AppHeaderWrapper>
     )
 })
--- src/components/app-header/style.js
+++ src/components/app-header/style.js
@@ -18,7 +18,7 @@ export const AppHeaderWrapper = styled.div`
 
 `
 
-export const headerLeft = styled.div`
+export const HeaderLeft = styled.div`
     display: flex;
     
     .logo{
@@ -29,9 +29,79 @@ export const headerLeft = styled.div`
         text-indent: -9999px; 
     }
 
+    .select-list {
+        display: flex;
+        line-height: 70px;
+    
+        .select-item {
+          position: relative;
+    
+          a {
+            display: block;
+            padding: 0 20px;
+            color: #ccc;
+          }
+    
+          :last-of-type a {
+            position: relative;
+            ::after {
+              position: absolute;
+              content: "";
+              width: 28px;
+              height: 19px;
+              background-image: url(${require("@/assets/img/sprite_01.png")});
+              background-position: -190px 0;
+              top: 20px;
+              right: -15px;
+            }
+          }
+    
+          &:hover a, .active {
+            color: #fff;
+            background: #000;
+            text-decoration: none;
+          }
+    
+          .active .icon {
+            position: absolute;
+            display: inline-block;
+            width: 12px;
+            height: 7px;
+            bottom: -1px;
+            left: 50%;
+            transform: translate(-50%, 0);
+            background-position: -226px 0;
+          }
+        }
+      }
 
 `
 
-export const headerRight = styled.div`
-   
+export const HeaderRight = styled.div`
+    display: flex;
+    align-items: center;
+    color: #ccc;
+    font-size: 12px;
+
+    .search {
+    width: 158px;
+    height: 32px;
+    border-radius: 16px;
+
+    input {
+        &::placeholder {
+        font-size: 12px;
+        }
+    }
+    }
+
+    .center {
+    width: 90px;
+    height: 32px;
+    line-height: 32px;
+    text-align: center;
+    border: 1px solid #666;
+    border-radius: 16px;
+    margin: 0 16px;
+    }
 `
